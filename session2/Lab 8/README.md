873ac19736624d2f38a1ca4da2e24a92e69a16e2
paolofloress44 <paolo.miguel.flores4@gmail.com>
2025-07-20 14:20:49 +0800

    Final Submission, Lab 8

---
+++ session2/Lab 8/README.md
@@ -0,0 +1,5 @@
+# Spring Boot Crashcourse
+## Paolo Miguel D. Flores
+### Lab 8: Create GET All Products Endpoint
+
+![Lab8Screenshot](/session2/Lab%208/Lab8.png)
---
+++ session2/Lab 8/src/main/java/com/example/demo/myfirstapi/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.demo.myfirstapi.controller;
+
+import com.example.demo.myfirstapi.model.Product; // Import your Product model
+import com.example.demo.myfirstapi.repository.ProductRepository; // Import your ProductRepository
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // This annotation makes it a REST controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products") // Maps HTTP GET requests to /products
+    public List<Product> getAllProducts() {
+        return productRepository.findAll(); // Call our repository to get all products
+    }
+}
